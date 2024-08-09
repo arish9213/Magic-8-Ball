1301eb454164821e47db729ee0e5cc2958111766
arish9213 <ariamas2021@gmail.com>
2024-08-09 12:09:42 -0230

    Update scribbles.py
    playing around with things

--- scribbles.py
+++ scribbles.py
@@ -1,9 +1,24 @@
 #welcome statement for users
 print("Welcome to my M8B game! Start thinking of a yes or no question, but first... \n")
+
 #ask for users name
 name = input("What's your name?: ")
+
 #greet user
 print("Hello", name, ", are you ready to ask a question?")
-# variable for continuing loop
 
+# variable for continuing loop
+another_question = True
+
+#set up while loop
+while another_question == True:
+    responses = random.choice(responses)
+    print(reponses)
+    more_qs = input("Do you have another question? Yes/No: ")
+
+    #set up if statment
+    if more_qs = "Yes":
+        #ask user for another question
+        next_q = input("Ask another question...")
+        print(next_q)
 
