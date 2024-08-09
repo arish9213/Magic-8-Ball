946fb6af100a2375f75ac3d075462b4f97915929
arish9213 <ariamas2021@gmail.com>
2024-08-09 11:57:06 -0230

    Create scribbles.py
    trying various methods

---
+++ scribbles.py
@@ -0,0 +1,9 @@
+#welcome statement for users
+print("Welcome to my M8B game! Start thinking of a yes or no question, but first... \n")
+#ask for users name
+name = input("What's your name?: ")
+#greet user
+print("Hello", name, ", are you ready to ask a question?")
+# variable for continuing loop
+
+
